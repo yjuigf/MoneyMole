7f7ea479b84f0358646ec3ed4d021aea5ab3b6dc
Harry, CHOI <hcho0050@student.monash.edu>
2022-01-22 19:40:03 +1100

    moneymole2

---
+++ MoneyMole2/app/src/main/java/com/example/moneymole2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.moneymole2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO: make this work
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
